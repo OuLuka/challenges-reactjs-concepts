6b50eb91ccd2cee09c47fdcb7b43c1d9275ec90e
ouluka <lfaria.gouvea@gmail.com>
2020-06-20 02:09:20 -0300

    second challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepos(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Omnistack ${Date.now()}`,
+      url: "gostack.com",
+      techs: ["React"],
+    });
+
+    const repo = response.data;
+    setRepos([...repos, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then((response) => {
+      const repoIndex = repos.findIndex((repo) => repo.id === id);
+      if (repoIndex < 0) {
+        console.log("Repo not found.");
+      }
+      repos.splice(repoIndex, 1);
+      setRepos([...repos]);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
